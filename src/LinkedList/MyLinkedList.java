cfa62c3e237d17c07f3af4e14e933999fe0bc8ba
Vaibhav <vaibhav.singh_cs18@gla.ac.in>
2020-02-21 00:15:51 +0530

    implementing methods of interface ContactADT to class MyLinkedList

--- src/LinkedList/MyLinkedList.java
+++ src/LinkedList/MyLinkedList.java
@@ -1,4 +1,25 @@
 package LinkedList;
 
-public class MyLinkedList {
+import ADT.ContactADT;
+
+public class MyLinkedList<E> implements ContactADT<E> {
+    @Override
+    public void add ( E item ) {
+
+    }
+
+    @Override
+    public E remove ( E item ) {
+        return null;
+    }
+
+    @Override
+    public int search ( E item ) {
+        return 0;
+    }
+
+    @Override
+    public void print () {
+
+    }
 }
