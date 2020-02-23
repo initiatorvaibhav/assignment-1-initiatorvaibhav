843fce4b217fe2dbfef87f403222a4181bf69e6a
Vaibhav <vaibhav.singh_cs18@gla.ac.in>
2020-02-23 22:00:16 +0530

    creating class name person to add contacts of a person implementing all the methods

---
+++ src/LinkedList/person.java
@@ -0,0 +1,25 @@
+package LinkedList;
+
+import ADT.PersonADT;
+
+public class person implements PersonADT {
+    @Override
+    public String personFirstName ( String item ) {
+        return null;
+    }
+
+    @Override
+    public String personLastName ( String item ) {
+        return null;
+    }
+
+    @Override
+    public String personContact ( String item ) {
+        return null;
+    }
+
+    @Override
+    public String emailID ( String item ) {
+        return null;
+    }
+}
