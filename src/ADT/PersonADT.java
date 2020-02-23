35221b4e358ff2ef1c20a4e22fcda76cdad5ab5a
Vaibhav <vaibhav.singh_cs18@gla.ac.in>
2020-02-23 21:54:20 +0530

    personFirstName method of type String will take First name of the person

---
+++ src/ADT/PersonADT.java
@@ -0,0 +1,12 @@
+package ADT;
+
+public interface PersonADT {
+    String personFirstName ( String item );
+
+    /*String personLastName(String item);
+
+    String personContact(String item);
+
+    String emailID(String item);*/
+
+}
